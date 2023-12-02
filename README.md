430fff0a974bbf6af7555c0fe5f080bedefd684d
Nativu5 <44155313+Nativu5@users.noreply.github.com>
2023-12-02 22:30:25 +0800

    :tada: Init with basic framework

---
+++ README.md
@@ -0,0 +1,9 @@
+# RepoMate - ChatGPT Plugin for GitHub Repos
+
+## Get started
+
+0. Sign up for [Cloudflare Workers](https://workers.dev). The free tier is more than enough for most use cases.
+1. Install [wrangler](https://developers.cloudflare.com/workers/cli-wrangler/install-update), the Cloudflare Workers CLI
+2. Clone this project in any development environment and install dependencies with `npm install`
+3. Run `wrangler login` to login to your Cloudflare account in wrangler
+4. Run `wrangler deploy` to publish the plugin to Cloudflare Workers
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import { GetTree } from './api/tree.js';
+import { GetSearch } from './api/search.js';
+import { GetContentSingle, GetContentBatch } from './api/content.js';
+
+import { OpenAPIRouter } from '@cloudflare/itty-router-openapi';
+
+export const router = OpenAPIRouter({
+	schema: {
+		info: {
+			title: 'RepoMate API',
+			description: 'A plugin that allows the user to access GitHub repositories using ChatGPT',
+			version: 'v0.0.1',
+		},
+	},
+	docs_url: '/',
+	aiPlugin: {
+		name_for_human: 'RepoMate',
+		name_for_model: 'github_repository_access',
+		description_for_human: 'GitHub Repositories Search & Access plugin for ChatGPT.',
+		description_for_model:
+			'GitHub Repositories Search & Access plugin for ChatGPT. You can use this plugin to search and access GitHub repositories, including their READMEs, project structures and so on.',
+		contact_email: 'root@naiv.fun',
+		legal_info_url: 'https://github.com/Nativu5/RepoMate/blob/main/LICENSE',
+		logo_url: 'https://workers.cloudflare.com/resources/logo/logo.svg',
+	},
+});
+
+// Define routes
+router.get('/api/tree', GetTree);
+router.get('/api/search', GetSearch);
+router.get('/api/content/single', GetContentSingle);
+router.get('/api/content/batch', GetContentBatch);
+
+// 404 for everything else
+router.all('*', () => new Response('Page Not Found. Please consult your client.', { status: 404 }));
+
+export default {
+	fetch: (...args) =>
+		router.handle(...args).catch((err) => {
+			// Log the error
+			console.log(err);
+			// Change 500 to 400
+			return new Response('Bad Request. Please request your client to revise the input.', { status: 400 });
+		}),
+};
